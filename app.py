439b268c4524f634d7a271eb2035b14e9ce2da1e
Hajer M <38040817+HajerMahjoub@users.noreply.github.com>
2020-06-02 19:53:15 +0100

    add the discordbot flask server
    @IamCathal  try to test and comment back your feedback will improve it more

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-from flask import Flask
-app = Flask(__name__)
+import discord
+import asyncio
 
-@app.route("/")
-def hello_world():
-   return "Hello, World!"
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/posts', methods=['POST'])
+def result():
+    print(request.form['sched'])
+    # Send a message to a discord text channel etc...
+    return 'Received !'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    content = message.content
+    fullUser = message.author.name+'#'+message.author.discriminator
+    print(str(message.timestamp)+" #"+message.channel.name+" "+fullUser+": "+str(content.encode('ascii', 'ignore').decode('ascii')))
+    if content.startswith('!'):
+
+        content = content[1:]
+        if content.startswith('test'):
+            counter = 0
+            tmp = await client.send_message(message.channel, 'Calculating messages...')
+            async for log in client.logs_from(message.channel, limit=100):
+                if log.author == message.author:
+                    counter += 1
+
+            await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+
+client.run('NzE3Mzg1NTE2NTMzODA5MjE0.XtZjmw.4JgvB046BCUKf6PPwRRvM_Oz0Ag')
