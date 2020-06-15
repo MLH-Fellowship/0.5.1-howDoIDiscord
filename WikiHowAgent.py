a5951a769a5fc39af52ae87451dc56a7ac926c19
rkbeatss <rkaus053@uottawa.ca>
2020-06-15 11:59:24 -0400

    modify input to help spaCy detect verbs

--- WikiHowAgent.py
+++ WikiHowAgent.py
@@ -1,47 +1,54 @@
 import spacy
 from pywikihow import WikiHow, search_wikihow
 
+
 def Question_generate(content):
-  verb  = []
+  verb = []
   noun = []
   nlp = spacy.load("en_core_web_sm")
   pos = content.lower().find("!howdoi")
-  Q_Token = nlp(content[pos+len("!howdoi"):])
-  for token in Q_Token :
+  sentence = "how to" + (content[pos+len("!howdoi"):])
+  Q_Token = nlp(sentence)
+  for token in Q_Token:
     print(token.pos_)
-    if token.pos_ in ["VERB" ,"NOUN","PROPN"] :
+    if token.pos_ in ["VERB", "NOUN", "PROPN"]:
       verb.append(token)
-  if len(verb) !=0:
+  print(verb)
+  if len(verb) != 0:
       v_pos = content.lower().find(verb[0].text)
       QUESTION = "how to "+content[v_pos:]
-  else :
+  else:
     QUESTION = "EMPTY"
   return QUESTION
 
 
-def WikiHowAgent(content) :
-  #parse the question from the message to howdoi 
+def WikiHowAgent(content):
+  #parse the question from the message to howdoi
   Question = Question_generate(content)
   #if no question was exist
   print(Question)
-  if Question == "EMPTY" :
+  if Question == "EMPTY":
     Answer = 0
-  #pywikihow to find one answer 
-  else :
-    how_tos = search_wikihow(Question,1)
+  #pywikihow to find one answer
+  else:
+    how_tos = search_wikihow(Question, 1)
     how_to = how_tos[0].as_dict()
     n_step = how_to['n_steps']
     steps = how_to['steps']
-    if n_step < 20 :
+    if n_step < 20:
       summary = []
-      for i in range(n_step) :
+      for i in range(n_step):
         summary.append(steps[i]['summary'])
-      summary ='\n'.join([str(elem) for elem in summary])
+      summary = '\n'.join([str(elem) for elem in summary])
       Answer = summary
-    else :
+    else:
       summary = []
-      for i in range(20) :
+      for i in range(20):
         summary.append(steps[i]['summary'])
-      summary ='\n'.join([str(elem) for elem in summary])
-      Answer = summary + " \n \n Check this link for more results: " + how_to['url']
+      summary = '\n'.join([str(elem) for elem in summary])
+      Answer = summary + \
+          " \n \n Check this link for more results: " + how_to['url']
   return Answer
+
+
+print(WikiHowAgent("!howdoi program in python"))
